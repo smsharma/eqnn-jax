b4d4c60f17c4080d02cc7cb35c2a5d0f33a02e49
Carol Cuesta <cuestalz@mit.edu>
2024-02-07 11:09:15 -0500

    tests for all work but segnn

--- tests/test_equivariance.py
+++ tests/test_equivariance.py
@@ -1,16 +1,18 @@
 import sys
+
 sys.path.append("../models")
 sys.path.append("../")
 import jax
 import numpy as np
 import flax.linen as nn
 import jraph
+from models.gnn import GNN
 from models.egnn import EGNN
+from models.segnn import SEGNN
+from e3nn_jax import IrrepsArray
 from utils.graph_utils import nearest_neighbors, rotate_representation
-from scipy.spatial import distance_matrix
 
 import pytest
-
 class GraphWrapper(nn.Module):
     model: nn.Module  # Specify the model as a class attribute
 
@@ -19,12 +21,20 @@ class GraphWrapper(nn.Module):
         model = jax.vmap(self.model)
         return model(x)
 
-def create_dummy_graph(node_features, k,):
-    sources, targets = jax.vmap(nearest_neighbors, in_axes=(0, None))(node_features[...,:3], k)
+def create_dummy_graph(
+    node_features,
+    k,
+    use_irreps=False,
+):
+    sources, targets = jax.vmap(nearest_neighbors, in_axes=(0, None))(
+        node_features[..., :3], k
+    )
     n_node = np.array([len(node_feat) for node_feat in node_features])
+    if use_irreps:
+        node_features = IrrepsArray("1o + 1o + 1x0e", node_features)
     return jraph.GraphsTuple(
         n_node=n_node,
-        n_edge=np.array(len(node_features)* [[k]]),
+        n_edge=np.array(len(node_features) * [[k]]),
         nodes=node_features,
         edges=None,
         globals=None,
@@ -33,49 +43,89 @@ def create_dummy_graph(node_features, k,):
     )
 
 
-
 @pytest.fixture
 def node_features():
-    return np.random.randn(2,5,7)
+    return np.random.randn(2, 5, 7)
 
-@pytest.fixture
-def dummy_graph(node_features):
-    return create_dummy_graph(
-        node_features=node_features,
-        k = 5,
-    )
+
 
 def apply_transformation(
-        x, angle_deg=45.,axis = np.array([0, 1 / np.sqrt(2), 1 / np.sqrt(2)])
-    ):
-    return jax.vmap(
-        rotate_representation, 
-        in_axes=(0,None,None,None))(
-            x, angle_deg, axis, False
-        )
-
-def transform_graph(
-        nodes,
-    ):
-    nodes = apply_transformation(nodes,)
-    return create_dummy_graph(
-        node_features=nodes,
-        k = 5,
+    x, angle_deg=45.0, axis=np.array([0, 1 / np.sqrt(2), 1 / np.sqrt(2)])
+):
+    return jax.vmap(rotate_representation, in_axes=(0, None, None, None))(
+        x, angle_deg, axis, False
     )
 
-def is_model_equivariant(data, model, params):
-    transformed_data = transform_graph(data.nodes)
+
+def transform_graph(
+    nodes,
+    k=5,
+    use_irreps=False,
+):
+    nodes = apply_transformation(
+        nodes,
+    )
+    return create_dummy_graph(
+        node_features=nodes,
+        k=k,
+        use_irreps=use_irreps,
+    )
+
+
+def is_model_equivariant(data, model, params, equivariant = True, use_irreps=False,):
+    transformed_data = transform_graph(data.nodes, use_irreps=use_irreps,)
     output_original = model.apply(params, data)
     output_original_transformed = apply_transformation(output_original.nodes)
     output_transformed = model.apply(params, transformed_data).nodes
-    assert np.allclose(output_transformed, output_original_transformed, rtol=5.e-2)
+    if equivariant:
+        assert np.allclose(output_transformed, output_original_transformed, rtol=5.0e-2)
+    assert ~np.allclose(output_transformed, output_original_transformed, rtol=5.0e-2)
+    
 
-
-def test_equivariant_segnn(dummy_graph):
-    model = GraphWrapper(EGNN(
-        message_passing_steps=3, d_hidden=32, n_layers=3, activation='gelu', tanh_out=True, soft_edges=True,
-        positions_only=False, task='node', decouple_pos_vel_updates=True,
-    ))
-    rng = jax.random.PRNGKey(0)  
+def test_not_equivariant_gnn(node_features,):
+    dummy_graph = create_dummy_graph(node_features=node_features,k=5,use_irreps=False,)
+    model = GraphWrapper(
+        GNN(
+            message_passing_steps=3,
+            d_hidden=32,
+            n_layers=3,
+            activation="gelu",
+            task="node",
+        )
+    )
+    rng = jax.random.PRNGKey(0)
     params = model.init(rng, dummy_graph)
-    is_model_equivariant(dummy_graph, model, params)
+    is_model_equivariant(dummy_graph, model, params, equivariant=False, use_irreps=False,)
+
+def test_equivariant_egnn(node_features):
+    dummy_graph = create_dummy_graph(node_features=node_features,k=5,use_irreps=False,)
+    model = GraphWrapper(
+        EGNN(
+            message_passing_steps=3,
+            d_hidden=32,
+            n_layers=3,
+            activation="gelu",
+            tanh_out=True,
+            soft_edges=True,
+            positions_only=False,
+            task="node",
+            decouple_pos_vel_updates=True,
+        )
+    )
+    rng = jax.random.PRNGKey(0)
+    params = model.init(rng, dummy_graph)
+    is_model_equivariant(dummy_graph, model, params, use_irreps=False,) 
+
+def test_equivariant_segnn(node_features):
+    dummy_graph = create_dummy_graph(node_features=node_features,k=5,use_irreps=True,)
+    model = GraphWrapper(
+        SEGNN(
+            num_message_passing_steps=3,
+            d_hidden=32,
+            task="node",
+            intermediate_hidden_irreps=False,
+        )
+    )
+    rng = jax.random.PRNGKey(0)
+    params = model.init(rng, dummy_graph)
+    is_model_equivariant(dummy_graph, model, params, use_irreps=True,) 
