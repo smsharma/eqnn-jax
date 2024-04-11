Fixed tpcf normalization

benchmarks/galaxies/dataset.py
@@ -37,8 +37,19 @@ class GalaxyDataset:
                 tpcfs_val = np.load(tpcf_dir / 'tpcfs_val.npy')
                 tpcfs_test = np.load(tpcf_dir / 'tpcfs_test.npy')
 
+            if use_tpcf == "small":
+                tpcf_idx = list(range(8))
+            elif use_tpcf == "large":
+                tpcf_idx = list(range(15, 24))
+            else:
+                tpcf_idx = list(range(24))
+
+            tpcfs_train = tpcfs_train[:, tpcf_idx]
+            tpcfs_val = tpcfs_val[:, tpcf_idx]
+            tpcfs_test = tpcfs_test[:, tpcf_idx]
+
             self.tpcfs_train, self.tpcfs_val, self.tpcfs_test = tpcfs_train, tpcfs_val, tpcfs_test
-            # self.tpcfs_train, self.tpcfs_val, self.tpcfs_test = self.normalize(tpcfs_train, tpcfs_val, tpcfs_test)
+            self.tpcfs_train, self.tpcfs_val, self.tpcfs_test = self.normalize(tpcfs_train, tpcfs_val, tpcfs_test)
 
         self.omega_m_train, self.omega_m_val, self.omega_m_test = self.load_labels(data_dir)
 
@@ -50,9 +61,9 @@ class GalaxyDataset:
         
         feat_idx = list(range(3))*use_pos + list(range(3, 6))*use_vel + [7]*use_mass
         
-        halos_train =  halos_train[:, :n_nodes, feat_idx]
-        halos_val =  halos_val[:, :n_nodes, feat_idx]
-        halos_test =  halos_test[:, :n_nodes, feat_idx]
+        halos_train =  halos_train[:, :n_nodes, feat_idx] / 1000.
+        halos_val =  halos_val[:, :n_nodes, feat_idx] / 1000.
+        halos_test =  halos_test[:, :n_nodes, feat_idx] / 1000.
         
         self.halos_mean = halos_train.mean((0,1))
         self.halos_std = halos_train.std((0,1))
@@ -73,8 +84,9 @@ class GalaxyDataset:
         
     
     def normalize(self, feats_train, feats_val, feats_test, eps=1e-8) -> Tuple[np.ndarray, ...]:
-        feats_mean = feats_train.mean((0,1))
-        feats_std = feats_train.std((0,1))
+        axes_except_last = tuple(range(feats_train.ndim - 1))
+        feats_mean = feats_train.mean(axes_except_last)
+        feats_std = feats_train.std(axes_except_last)
     
         feats_train = (feats_train - feats_mean) / (feats_std + eps)
         feats_val = (feats_val - feats_mean) / (feats_std + eps)

benchmarks/galaxies/full_batch_train.py
@@ -21,7 +21,6 @@ from jax.experimental.sparse import BCOO
 import e3nn_jax as e3nn
 
 import sys
-
 sys.path.append("../../")
 
 from models.utils.graph_utils import build_graph, get_apply_pbc
@@ -31,17 +30,11 @@ from models.utils.equivariant_graph_utils import get_equivariant_graph
 from models.mlp import MLP
 from models.gnn import GNN
 from models.egnn import EGNN
-
 # from models.equivariant_transformer import EquivariantTransformer
 from models.segnn import SEGNN
 from models.diffpool import DiffPool
 
-from benchmarks.galaxies.dataset import GalaxyDataset
-
-MLP_PARAMS = {
-    "feature_sizes": [128, 128, 128, 1],
-}
-
+from benchmarks.galaxies.tf_dataset import get_halo_dataset
 
 GNN_PARAMS = {
     "n_outputs": 1,
@@ -79,10 +72,10 @@ EGNN_PARAMS = {
 
 DIFFPOOL_PARAMS = {
     "n_downsamples": 2,
-    "d_downsampling_factor": 10,
+    "d_downsampling_factor": 5,
     "k": 15,
     "gnn_kwargs": GNN_PARAMS,
-    "combine_hierarchies_method": "mean",
+    "combine_hierarchies_method": "concat",
     "use_edge_features": True,
     "task": "graph",
     "mlp_readout_widths": [8, 2, 2],
@@ -92,21 +85,19 @@ DIFFPOOL_PARAMS = {
 SEGNN_PARAMS = {
     "d_hidden": 64,
     "l_max_hidden": 1,
-    "num_blocks": 2,
-    "num_message_passing_steps": 3,
+    "num_blocks": 3,
+    "num_message_passing_steps": 2,
     "intermediate_hidden_irreps": True,
     "task": "graph",
     "output_irreps": e3nn.Irreps("1x0e"),
-    "hidden_irreps": weight_balanced_irreps(lmax=1,
-                                            scalar_units=64,
-                                            irreps_right=e3nn.Irreps.spherical_harmonics(1),
-                                            ),
+    "hidden_irreps": weight_balanced_irreps(
+        lmax=1,
+        scalar_units=64,
+        irreps_right=e3nn.Irreps.spherical_harmonics(1),
+    ),
     "normalize_messages": True,
     "message_passing_agg": "mean",
-    "readout_agg": "mean",
-    "mlp_readout_widths": [8, 2, 2],
 }
-
 
 class GraphWrapper(nn.Module):
     model_name: str
@@ -117,8 +108,6 @@ class GraphWrapper(nn.Module):
     def __call__(self, x):
         if self.model_name == "DeepSets":
             raise NotImplementedError
-        elif self.model_name == "MLP":
-            return jax.vmap(MLP(**self.param_dict))(x.globals)
         elif self.model_name == "GNN":
             return jax.vmap(GNN(**self.param_dict))(x)
         elif self.model_name == "EGNN":
@@ -230,26 +219,24 @@ def split_batches(num_local_devices, halo_batch, omega_m_batch, tpcfs_batch):
     return halo_batch, omega_m_batch, tpcfs_batch
 
 
-def run_expt(
-    model_name,
-    feats,
-    param_dict,
-    data_dir,
-    use_pbc=True,
-    use_edges=True,
-    use_rbf=False,
-    use_tpcf="none",
-    n_steps=1000,
-    n_batch=32,
-    n_train=1600,
-    n_test_batch=32,
-    learning_rate=5e-5,
-    weight_decay=1e-5,
-    eval_every=200,
-    get_node_reps=False,
-    plotting=True,
-):
-
+def run_expt(model_name,
+             feats,
+             param_dict, 
+             data_dir,
+             use_pbc = True, 
+             use_edges = True, 
+             use_rbf = False,
+             use_tpcf = 'none',
+             n_steps = 1000,
+             n_batch = 32,
+             n_train = 1600,
+             n_test_batch = 32,
+             learning_rate = 5e-5, 
+             weight_decay = 1e-5,
+             eval_every = 200,
+             get_node_reps = False,
+             plotting=True):
+    
     # Create experiment directory
     experiments_base_dir = Path(__file__).parent / "experiments/"
     d_hidden = param_dict["d_hidden"]
@@ -258,132 +245,132 @@ def run_expt(
         + use_tpcf
         + "_rbf" * use_rbf
     )
-
+               
     current_experiment_dir = experiments_base_dir / experiment_id
     current_experiment_dir.mkdir(parents=True, exist_ok=True)
 
-    print("Loading dataset...")
-    if feats == "pos":
-        D = GalaxyDataset(data_dir, use_pos=True, use_vel=False, use_tpcf=use_tpcf)
-    elif feats == "all":
-        D = GalaxyDataset(data_dir, use_pos=True, use_vel=True, use_tpcf=use_tpcf)
+    print('Loading dataset...')
+    if feats == 'pos':
+        features = ['x', 'y', 'z']
+    elif feats == 'all':
+        features = ['x', 'y', 'z', 'vx', 'vy', 'vz']
     else:
         raise NotImplementedError
+    targets = ['Omega_m']
+        
+    train_dataset, n_train, mean, std, _, _ = get_halo_dataset(batch_size=n_batch,  # Batch size
+                                                                num_samples=n_train,  # If not None, will only take a subset of the dataset
+                                                                split='train',  # 'train', 'val', 'test'
+                                                                standardize=True,  # If True, will standardize the features
+                                                                return_mean_std=True,  # If True, will return (dataset, num_total, mean, std, mean_params, std_params), else (dataset, num_total)
+                                                                seed=42,  # Random seed
+                                                                features=features,  # Features to include
+                                                                params=targets,  # Parameters to include
+                                                                use_tpcf=use_tpcf
+                                                            )
+    mean, std = mean.numpy(), std.numpy()
+    norm_dict = {'mean': mean, 'std': std}
+    train_iter = iter(train_dataset)
+    halo_train, omega_m_train = next(train_iter)
 
-    halo_train, halo_val, halo_test = D.halos_train, D.halos_val, D.halos_test
-    num_train_sims = halo_train.shape[0]
-    mean, std = D.halos_mean, D.halos_std
+    val_dataset, n_val = get_halo_dataset(batch_size=200,  
+                                           num_samples=400, 
+                                           split='val',
+                                           standardize=True, 
+                                           return_mean_std=False,  
+                                           seed=42,
+                                           features=features, 
+                                           params=targets,
+                                           use_tpcf=use_tpcf
+                                        )
+    val_iter = iter(val_dataset)
+    halo_val, omega_m_val = next(val_iter)
+    halo_test, omega_m_test = next(val_iter)
 
+    # Convert to numpy arrays
+    halo_train, omega_m_train = halo_train.numpy(), omega_m_train.numpy()
+    halo_val, omega_m_val = halo_val.numpy(), omega_m_val.numpy()
+    halo_test, omega_m_test = halo_test.numpy(), omega_m_test.numpy()
+    
+    tpcfs_train = None
+    tpcfs_val = None
+    tpcfs_test = None
+    
     apply_pbc = get_apply_pbc(std=std) if use_pbc else None
-
-    omega_m_train, omega_m_val, omega_m_test = (
-        D.omega_m_train,
-        D.omega_m_val,
-        D.omega_m_test,
-    )
-
-    if use_tpcf != "none":
-        tpcfs_train = D.tpcfs_train
-        tpcfs_val = D.tpcfs_val
-        tpcfs_test = D.tpcfs_test
-
-        graph = build_graph(
-            halo_train[:2],
-            tpcfs_train[:2],
-            k=K,
-            apply_pbc=apply_pbc,
-            use_edges=use_edges,
-            use_rbf=use_rbf
-        )
-    else:
-        tpcfs_train = None
-        tpcfs_val = None
-        tpcfs_test = None
-
-        graph = build_graph(
-            halo_train[:2],
-            None,
-            k=K,
-            apply_pbc=apply_pbc,
-            use_edges=use_edges,
-            use_rbf=use_rbf
-        )
-
 
     # Split eval batches across devices
     num_local_devices = jax.local_device_count()
-    halo_val, omega_m_val, tpcfs_val = split_batches(
-        num_local_devices, halo_val, omega_m_val, tpcfs_val
-    )
-    halo_test, omega_m_test, tpcfs_test = split_batches(
-        num_local_devices, halo_test, omega_m_test, tpcfs_test
-    )
+    halo_val, omega_m_val, tpcfs_val = split_batches(num_local_devices, halo_val, omega_m_val, tpcfs_val)
+    halo_test, omega_m_test, tpcfs_test = split_batches(num_local_devices, halo_test, omega_m_test, tpcfs_test)
 
     if get_node_reps:
-        param_dict["get_node_reps"] = True
+        param_dict['get_node_reps'] = True
 
+    graph = build_graph(halo_train[:2], 
+                        None, 
+                        k=K, 
+                        apply_pbc=apply_pbc, 
+                        use_edges=use_edges, 
+                        use_rbf=use_rbf)
+    
     model = GraphWrapper(model_name, param_dict, apply_pbc)
     key = jax.random.PRNGKey(0)
     out, params = model.init_with_output(key, graph)
-
+    
     # Define train state and replicate across devices
     replicate = flax.jax_utils.replicate
     unreplicate = flax.jax_utils.unreplicate
     tx = optax.adamw(learning_rate=learning_rate, weight_decay=weight_decay)
     state = TrainState.create(apply_fn=model.apply, params=params, tx=tx)
     pstate = replicate(state)
-
+    
     # Run training loop
-    print("Training...")
+    print('Training...')
     losses = []
     val_losses = []
     best_val = 1e10
     with trange(n_steps, ncols=120) as steps:
         for step in steps:
-            key, subkey = jax.random.split(key)
-            idx = jax.random.choice(key, num_train_sims, shape=(n_batch,))
-
-            halo_batch = halo_train[:n_train][idx]
-            omega_m_batch = omega_m_train[:n_train][idx]
-            if use_tpcf != "none":
-                tpcfs_batch = tpcfs_train[idx]
-            else:
+            train_iter = iter(train_dataset)
+            running_train_loss = 0.0
+            for _ in range(n_train // n_batch):
+                halo_batch, omega_m_batch = next(train_iter)
+                halo_batch, omega_m_batch = halo_batch.numpy(), omega_m_batch.numpy()
                 tpcfs_batch = None
-
-            # Split batches across devices
-            halo_batch, omega_m_batch, tpcfs_batch = split_batches(
-                num_local_devices, halo_batch, omega_m_batch, tpcfs_batch
-            )
-            pstate, metrics = train_step(
-                pstate, halo_batch, omega_m_batch, tpcfs_batch, apply_pbc, use_rbf
-            )
-            train_loss = unreplicate(metrics["loss"])
-
-            if step % eval_every == 0:
-                outputs, val_metrics = eval_step(
-                    pstate, halo_val, omega_m_val, tpcfs_val, apply_pbc, use_rbf
+            
+                # Split batches across devices
+                halo_batch, omega_m_batch, tpcfs_batch = split_batches(
+                    num_local_devices, halo_batch, omega_m_batch, tpcfs_batch
                 )
-                val_loss = unreplicate(val_metrics["loss"])
-
-                if val_loss < best_val:
-                    best_val = val_loss
-                    tag = " (best)"
-
-                    outputs, test_metrics = eval_step(
-                        pstate, halo_test, omega_m_test, tpcfs_test, apply_pbc, use_rbf
-                    )
-                    test_loss_ckp = unreplicate(test_metrics["loss"])
-                else:
-                    tag = ""
-
-            steps.set_postfix_str(
-                "loss: {:.5f}, val_loss: {:.5f}, ckp_test_loss: {:.5F}".format(
-                    train_loss, val_loss, test_loss_ckp
+                pstate, metrics = train_step(
+                    pstate, halo_batch, omega_m_batch, tpcfs_batch, apply_pbc, use_rbf
                 )
+                train_loss = unreplicate(metrics["loss"])
+                running_train_loss += train_loss
+            avg_train_loss = running_train_loss/(n_train // n_batch)
+            
+            outputs, val_metrics = eval_step(
+                pstate, halo_val, omega_m_val, tpcfs_val, apply_pbc, use_rbf
             )
+            val_loss = unreplicate(val_metrics["loss"])
+
+            if val_loss < best_val:
+                best_val = val_loss
+                tag = " (best)"
+
+                outputs, test_metrics = eval_step(
+                    pstate, halo_test, omega_m_test, tpcfs_test, apply_pbc, use_rbf
+                )
+                test_loss_ckp = unreplicate(test_metrics["loss"])
+            else:
+                tag = ""
+            
+            steps.set_postfix_str('avg loss: {:.5f}, val_loss: {:.5f}, ckp_test_loss: {:.5F}'.format(avg_train_loss,
+                                                                                                   val_loss,
+                                                                                                   test_loss_ckp))
             losses.append(train_loss)
             val_losses.append(val_loss)
-
+            
         outputs, test_metrics = eval_step(
             pstate, halo_test, omega_m_test, tpcfs_test, apply_pbc, use_rbf
         )
@@ -392,87 +379,67 @@ def run_expt(
             "Training done.\n"
             f"Final test loss {test_loss:.6f} - Checkpoint test loss {test_loss_ckp:.6f}.\n"
         )
-
+        
     if plotting:
-        plt.scatter(np.vstack(omega_m_test), outputs, color="firebrick")
-        plt.plot(np.vstack(omega_m_test), np.vstack(omega_m_test), color="gray")
-        plt.title("True vs. predicted Omega_m")
-        plt.xlabel("True")
-        plt.ylabel("Predicted")
+        plt.scatter(np.vstack(omega_m_test), outputs, color='firebrick')
+        plt.plot(np.vstack(omega_m_test), np.vstack(omega_m_test), color='gray')
+        plt.title('True vs. predicted Omega_m')
+        plt.xlabel('True')
+        plt.ylabel('Predicted')
         plt.savefig(current_experiment_dir / "omega_m_preds.png")
-
+        
     np.save(current_experiment_dir / "train_losses.npy", losses)
     np.save(current_experiment_dir / "val_losses.npy", val_losses)
-
-
+    
+    
 def main(model, feats, lr, decay, steps, batch_size, use_rbf, use_tpcf, k):
-    if model == 'MLP':
-        MLP_PARAMS['d_hidden'] = MLP_PARAMS['feature_sizes'][0]
-        params = MLP_PARAMS
-    elif model == "GNN":
+    if model == 'GNN':
         params = GNN_PARAMS
-    elif model == "EGNN":
+    elif model == 'EGNN':
         params = EGNN_PARAMS
-    elif model == "SEGNN":
+    elif model == 'SEGNN':
         params = SEGNN_PARAMS
-    elif model == "DiffPool":
+    elif model == 'DiffPool':
         # GNN_PARAMS['task'] = 'node'
-        DIFFPOOL_PARAMS["gnn_kwargs"] = {
-            "d_hidden": 64,
-            "d_output": 16,
-            "n_layers": 2,
-            "message_passing_steps": 2,
-            "task": "node",
-        }
-        DIFFPOOL_PARAMS["d_hidden"] = DIFFPOOL_PARAMS["gnn_kwargs"]["d_hidden"]
+        DIFFPOOL_PARAMS['gnn_kwargs'] = {"d_hidden": 64, 
+                                        "d_output": 16, 
+                                        "n_layers": 2, 
+                                        "message_passing_steps":2, 
+                                        "task": 'node'}
         params = DIFFPOOL_PARAMS
     else:
         raise NotImplementedError
-
-    data_dir = Path("/n/holystore01/LABS/iaifi_lab/Lab/set-diffuser-data/val_split/")
-
-    run_expt(
-        model,
-        feats,
-        params,
-        data_dir,
-        learning_rate=lr,
-        weight_decay=decay,
-        n_steps=steps,
-        n_batch=batch_size,
-        use_rbf=use_rbf,
-        use_tpcf=use_tpcf,
-    )
-
+        
+    data_dir = Path('/n/holystore01/LABS/iaifi_lab/Lab/set-diffuser-data/val_split/')
+    
+    run_expt(model, 
+             feats, 
+             params, 
+             data_dir, 
+             learning_rate=lr, 
+             weight_decay=decay, 
+             n_steps=steps, 
+             n_batch=batch_size, 
+             use_rbf=use_rbf,
+             use_tpcf=use_tpcf)  
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model", help="Name of model", default="GNN")
-    parser.add_argument(
-        "--feats", help="Features to use", default="pos", choices=["pos", "all"]
-    )
-    parser.add_argument("--lr", type=float, help="Learning rate", default=1e-4)
-    parser.add_argument("--decay", type=float, help="Weight decay", default=1e-5)
-    parser.add_argument("--steps", type=int, help="Number of steps", default=5000)
-    parser.add_argument("--batch_size", type=int, help="Batch size", default=32)
-    parser.add_argument(
-        "--use_rbf",
-        type=bool,
-        help="Whether to include RBF kernel for edge features",
-        default=False,
-    )
-    parser.add_argument(
-        "--use_tpcf",
-        type=str,
-        help="Which tpcf features to include",
-        default="none",
-        choices=["none", "small", "large", "all"],
-    )
-    parser.add_argument(
-        "--k", type=int, help="Number of neighbors for kNN graph", default=20
-    )
+    parser.add_argument('--model', help='Name of model', default='GNN')
+    parser.add_argument('--feats', help='Features to use', default='pos', choices=['pos', 'all'])
+    parser.add_argument('--lr', type=float,help='Learning rate', default=1e-4)
+    parser.add_argument('--decay', type=float, help='Weight decay', default=1e-5)
+    parser.add_argument('--steps', type=int, help='Number of steps', default=5000)
+    parser.add_argument('--batch_size', help='Batch size', default=32)
+    parser.add_argument('--use_rbf', type=bool, help='Whether to include RBF kernel for edge features', default=False)
+    parser.add_argument('--use_tpcf', type=str, help='Which tpcf features to include', default='none', choices=['none', 'small', 'large', 'all'])
+    parser.add_argument('--k', type=int, help='Number of neighbors for kNN graph', default=20)
     args = parser.parse_args()
-
+    
     K = args.k
-
+    
     main(**vars(args))
+    
+
+    
+    

benchmarks/galaxies/tf_dataset.py
@@ -55,7 +55,7 @@ def _parse_function(proto, features=['x', 'y', 'z', 'Jx', 'Jy', 'Jz', 'vx', 'vy'
 
     # Stack the feature tensors to create a single tensor
     # Each tensor must have the same shape
-    stacked_features = tf.stack(feature_tensors, axis=1)  # Creates a [num_points, num_features] tensor
+    stacked_features = tf.stack(feature_tensors, axis=1) / 1000.  # Creates a [num_points, num_features] tensor
     stacked_params = tf.stack(param_tensors, axis=0)  # Creates a [num_params] tensor
 
     return stacked_features, stacked_params
@@ -83,13 +83,14 @@ def get_halo_dataset(batch_size=64,
     dataset = dataset.map(partial(_parse_function, features=features, params=params))
 
     # Get mean and std as tf arrays
-    mean = tf.constant([MEAN_HALOS_DICT[f] for f in features], dtype=tf.float32)
-    std = tf.constant([STD_HALOS_DICT[f] for f in features], dtype=tf.float32)
+    mean = tf.constant([MEAN_HALOS_DICT[f] for f in features], dtype=tf.float32) / 1000.
+    std = tf.constant([STD_HALOS_DICT[f] for f in features], dtype=tf.float32) / 1000.
     mean_params = tf.constant([MEAN_PARAMS_DICT[f] for f in params], dtype=tf.float32)
     std_params = tf.constant([STD_PARAMS_DICT[f] for f in params], dtype=tf.float32)
 
     if standardize:
-        dataset = dataset.map(lambda x, p: ((x - mean) / std, (p - mean_params) / std_params))
+        # dataset = dataset.map(lambda x, p: ((x - mean) / std, (p - mean_params) / std_params))
+        dataset = dataset.map(lambda x, p: ((x - mean) / std, p))
 
     dataset = dataset.batch(batch_size)
     if split == 'train':

models/gnn.py
@@ -116,9 +116,7 @@ class GNN(nn.Module):
         Returns:
             jraph.GraphsTuple: Updated graph
         """
-
         processed_graphs = graphs
-
         # if processed_graphs.globals is not None:
         #     processed_graphs = processed_graphs._replace(
         #         globals=processed_graphs.globals.reshape(1, -1)
@@ -188,20 +186,20 @@ class GNN(nn.Module):
                 agg_nodes = readout_agg_fn(processed_graphs.nodes[:, :3], axis=0)
             else:
                 agg_nodes = readout_agg_fn(processed_graphs.nodes, axis=0)
+
                 
             if processed_graphs.globals is not None:
                 agg_nodes = jnp.concatenate([agg_nodes, processed_graphs.globals]) #use tpcf
+                
                 norm = nn.LayerNorm()
                 agg_nodes = norm(agg_nodes)
-
+                
             # Readout and return
-            mlp = MLP([self.mlp_readout_widths[0] * agg_nodes.shape[-1]] + [w * self.d_hidden for w in self.mlp_readout_widths[1:]] + [self.n_outputs,])                                                                        
-            # out = MLP(
-            #     [w * self.d_hidden for w in self.mlp_readout_widths]
-            #     + [
-            #         self.n_outputs,
-            #     ]
-            # )(agg_nodes)
+            mlp = MLP([
+                self.mlp_readout_widths[0] * agg_nodes.shape[-1]] + \
+                [w * self.d_hidden for w in self.mlp_readout_widths[1:]] + \
+                [self.n_outputs,]
+            )                                                                        
             out = mlp(agg_nodes)                                                             
             return out
 

models/utils/equivariant_graph_utils.py
@@ -1,4 +1,4 @@
-from typing import Any, NamedTuple, Iterable, Mapping, Union, Optional
+from typing import Any, NamedTuple, Iterable, Mapping, Union, Optional, Callable
 
 import e3nn_jax as e3nn
 from e3nn_jax import IrrepsArray
@@ -6,7 +6,7 @@ from e3nn_jax import IrrepsArray
 import jax
 from jraph import segment_mean
 import jax.numpy as jnp
-from .graph_utils import apply_pbc
+from .graph_utils import get_apply_pbc
 
 ArrayTree = Union[jnp.ndarray, Iterable["ArrayTree"], Mapping[Any, "ArrayTree"]]
 
@@ -38,32 +38,17 @@ def get_equivariant_graph(
     additional_messages: Optional[jnp.ndarray] = None,
     steerable_velocities: bool = False,
     spherical_harmonics_norm="integral",
-    periodic_boundaries: bool = False,
-    norm_dict=None,
-    unit_cell=None,
-    boxsize=1000.
+    apply_pbc: Optional[Callable] = None,
 ):
     attribute_irreps = e3nn.Irreps.spherical_harmonics(lmax_attributes)
     x_i = positions[jnp.arange(positions.shape[0])[:, None], senders, :]
     x_j = positions[jnp.arange(positions.shape[0])[:, None], receivers, :]
-    if periodic_boundaries:
-        std = norm_dict["std"][None, :3][0]
-        x_i = x_i.array + std
-        x_j = x_j.array + std
-        r_ij = x_i - x_j
-    
-        cell = jnp.diag(boxsize/std)
-        print('r_ij:')
-        print(r_ij)
-        print()
-        print('cell:')
-        print(cell)
-        
-        r_ij = apply_pbc(r_ij, cell)
-        print('r_ij after pbc:')
-        print(r_ij)
+    if apply_pbc is not None:
+        r_ij = x_i.array - x_j.array
+        r_ij = apply_pbc(r_ij,)
     else:
-        r_ij = x_i - x_j
+        r_ij = (x_i - x_j).array
+    # print(r_ij)
     r_ij = IrrepsArray("1o", r_ij)
 
     steerable_edge_attrs = e3nn.spherical_harmonics(
@@ -71,7 +56,7 @@ def get_equivariant_graph(
         input=r_ij,
         normalize=True,
         normalization=spherical_harmonics_norm,
-    ) # check histograms of a single graphs
+    )  # check histograms of a single graphs
 
     # print(f'steerable_edge_attrs Mean: {jnp.mean(steerable_edge_attrs.array, axis=(0,1))} Std: {jnp.std(steerable_edge_attrs.array, axis=(0,1))}')
 
@@ -98,16 +83,19 @@ def get_equivariant_graph(
             normalization=spherical_harmonics_norm,
         )
         steerable_node_attrs += vel_sph
-    
-    additional_messages = e3nn.IrrepsArray("1x0e", 
-                                            jnp.sqrt(jnp.sum(r_ij.array**2, axis=-1, keepdims=True)))
-    
+
+    additional_messages = e3nn.IrrepsArray(
+        "1x0e", jnp.sqrt(jnp.sum(r_ij.array**2, axis=-1, keepdims=True))
+    )
+
     # print(f'additional_messages Mean: {jnp.mean(additional_messages.array, axis=0)} Std: {jnp.std(additional_messages.array, axis=0)}')
     # print('additional_messages shape:', additional_messages.array.shape)
 
-    print(f'node_features Mean: {jnp.mean(node_features.array, axis=(0,1))} Std: {jnp.std(node_features.array, axis=(0,1))}')
-    print('Node features:')
-    print(node_features)
+    # print(
+    #     f"node_features Mean: {jnp.mean(node_features.array, axis=(0,1))} Std: {jnp.std(node_features.array, axis=(0,1))}"
+    # )
+    # print("Node features:")
+    # print(node_features)
 
     return SteerableGraphsTuple(
         nodes=node_features,

models/utils/graph_utils.py
@@ -2,42 +2,44 @@ import jax
 import jax.numpy as np
 import jraph
 
+from typing import Callable, Optional
 from functools import partial
 
 EPS = 1e-7
 
-def apply_pbc(
-    dr: np.array,
-    cell: np.array = np.array(
+def get_apply_pbc(std: np.array=None, cell: np.array = np.array(
         [
-            [
-                1.0,
-                0.0,
-                0.0,
-            ],
+            [1.0, 0.0, 0.0],
             [0.0, 1.0, 0.0],
             [0.0, 0.0, 1.0],
         ]
     ),
-) -> np.array:
-    """Apply periodic boundary conditions to a displacement vector, dr, given a cell.
+):
+    if std is not None:
+        cell = cell / std[:3]
+        cell -= 0.5 / std[:3]
 
-    Args:
-        dr (np.array): An array of shape (N,3) containing the displacement vector
-        cell_matrix (np.array): A 3x3 matrix describing the box dimensions and orientation.
+    def apply_pbc(
+        dr: np.array,
+    ) -> np.array:
+        """Apply periodic boundary conditions to a displacement vector, dr, given a cell.
 
-    Returns:
-        np.array: displacement vector with periodic boundary conditions applied
-    """
-    return dr - np.round(dr.dot(np.linalg.inv(cell))).dot(cell)
+        Args:
+            dr (np.array): An array of shape (N,3) containing the displacement vector
+            cell_matrix (np.array): A 3x3 matrix describing the box dimensions and orientation.
+
+        Returns:
+            np.array: displacement vector with periodic boundary conditions applied
+        """
+        return (dr - np.round(dr.dot(np.linalg.inv(cell))).dot(cell)) 
+    return apply_pbc
 
 
-@partial(jax.jit, static_argnums=(1,))
+@partial(jax.jit, static_argnums=(1,2))
 def nearest_neighbors(
     x: np.array,
     k: int,
-    cell: np.array = None,
-    mask: np.array = None,
+    apply_pbc: Optional[Callable] = None,
 ):
     """Returns the nearest neighbors of each node in x.
 
@@ -53,80 +55,55 @@ def nearest_neighbors(
     n_nodes = x.shape[0]
     # Compute the vector difference between positions
     dr = (x[:, None, :] - x[None, :, :]) + EPS
-    if cell != None:
-        dr = apply_pbc(
-            dr=dr,
-            cell=cell,
-        )
-
-    # Calculate the distance matrix 
+    if apply_pbc is not None:
+        dr = apply_pbc(dr)
+    # Calculate the distance matrix
     distance_matrix = np.sum(dr**2, axis=-1)
-
     # Get indices of nearest neighbors
     indices = np.argsort(distance_matrix, axis=-1)[:, :k]
 
     # Create sources and targets arrays
     sources = np.repeat(np.arange(n_nodes), k)
     targets = indices.ravel()
-
     return sources, targets, dr[sources, targets]
 
-def build_graph(halos, 
-                tpcfs, 
-                k, 
-                use_pbc=True, 
-                use_edges=True, 
-                boxsize=1000., 
-                unit_cell = np.array([[1.,0.,0.,],[0.,1.,0.], [0.,0.,1.]]), 
-                mean=None, 
-                std=None,
-                use_rbf=False,
-                sigma_num=8):
-    
-    # if mean is not None and std is not None:
-    #     halos = halos * std + mean
-    #     halos /= boxsize
 
+def build_graph(
+    halos,
+    tpcfs,
+    k,
+    use_edges=True,
+    apply_pbc: Optional[Callable] = None,
+    use_rbf=False,
+    sigma_num=16,
+):
     n_batch = len(halos)
-    
-    if use_pbc:
-        if std is not None:
-            halos += std[:3]
-            cell = np.diag(boxsize/std[:3])
-        else:
-            cell = np.array([[1.,0.,0.,],[0.,1.,0.], [0.,0.,1.]])
-    else:
-        cell = None
-        
-    sources, targets, distances = jax.vmap(partial(nearest_neighbors), in_axes=(0, None, None))(halos[..., :3], k, cell)
-    if use_pbc and std is not None:
-        halos -= std[:3]
-    
+
+    sources, targets, distances = jax.vmap(
+        partial(nearest_neighbors), in_axes=(0, None, None)
+    )(halos[..., :3], k, apply_pbc,)
+
     if use_edges:
-        edges = np.sqrt(np.sum(distances **2, axis=-1, keepdims=True))
+        edges = np.sqrt(np.sum(distances**2, axis=-1, keepdims=True))
         if use_rbf:
             min_sigma = np.min(edges)
             max_sigma = np.mean(edges)
-            sigma_vals = np.linspace(min_sigma, max_sigma, num=sigma_num) 
-            edges = [np.exp(- edges**2 / (2 * sigma**2)) for sigma in sigma_vals]
+            sigma_vals = np.linspace(min_sigma, max_sigma, num=sigma_num)
+            edges = [np.exp(-(edges**2) / (2 * sigma**2)) for sigma in sigma_vals]
             edges = np.concatenate(edges, axis=-1)
     else:
         edges = None
 
-    print('node features')
-    print(halos)
-    print('edge features')
-    print(edges.shape)
-    
     return jraph.GraphsTuple(
-            n_node=np.array([[halos.shape[1]]]*n_batch),
-            n_edge=np.array(n_batch * [[k]]),
-            nodes=halos,
-            edges=edges,
-            globals=tpcfs,
-            senders=sources,
-            receivers=targets,
-        )
+        n_node=np.array([[halos.shape[1]]] * n_batch),
+        n_edge=np.array(n_batch * [[k]]),
+        nodes=halos,
+        edges=edges,
+        globals=tpcfs,
+        senders=sources,
+        receivers=targets,
+    )
+
 
 def add_graphs_tuples(
     graphs: jraph.GraphsTuple, other_graphs: jraph.GraphsTuple